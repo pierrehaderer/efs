e4614972a07c6f952ca09fdcb9e4b23d1c9ccb6e
pierre.haderer <pierrehaderer@yahoo.fr>
2016-10-12 00:17:07 +0200

    Canvas is now streched and ready for the next steps.

--- platforms/browser/www/js/index.js
+++ platforms/browser/www/js/index.js
@@ -1,4 +1,5 @@
-var ctx = document.getElementById("ctx").getContext("2d");
+var canvas = document.getElementById("ctx");
+var ctx = canvas.getContext("2d");
 
 var app = {
     position: 0,
@@ -6,13 +7,17 @@ var app = {
     // Application Constructor
     initialize: function() {
         document.addEventListener('deviceready', this.onDeviceReady, false);
-        document.addEventListener('click', this.onDeviceReady, false);
-        document.addEventListener('touchstart', this.updateThenDraw, false);
+        document.addEventListener('keydown', this.onDeviceReady, false);
+        //document.addEventListener('click', this.updateThenDraw, false);
+        //document.addEventListener('touchstart', this.updateThenDraw, false);
     },
 
     // deviceready Event Handler
     onDeviceReady: function() {
-        alert("device is ready");
+        canvas.style.visibility = "visible";
+        canvas.setAttribute("width", document.body.clientWidth);
+        canvas.setAttribute("height", document.body.clientHeight);
+        setInterval(app.updateThenDraw, 40)
     },
     
     updateThenDraw: function() {
@@ -21,10 +26,11 @@ var app = {
     },
     
     update: function() {
-        this.position += 1;
+        this.position += 10;
     },
-        
+
     draw: function() {
-        ctx.fillText('Pierre', this.position, this.position);
+        var text = document.body.clientWidth.toString() + '-' + document.body.clientHeight.toString();
+        ctx.fillText(text, this.position, this.position);
     }
 };
