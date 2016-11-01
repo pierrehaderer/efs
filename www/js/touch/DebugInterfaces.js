f65393b2bd6885370363e444c64cd2d046cc0b34
pierre.haderer <pierrehaderer@yahoo.fr>
2016-11-01 01:23:00 +0100

    Correct DebugInterfaces for maps

--- www/js/touch/DebugInterfaces.js
+++ www/js/touch/DebugInterfaces.js
@@ -25,7 +25,7 @@ DebugInterfaces.prototype.draw = function() {
         ctx.fillRect(canvasWidth - 200, 0, 200, 80);
         ctx.fillStyle = 'white';
         ctx.font = '16px Arial';
-        ctx.fillText("Map(x,y) = (" + app.maps.toString() + "," + app.maps.toString() + ")", canvasWidth - 195, 15);
+        ctx.fillText("Map(x,y) = (" + app.maps.x.toString() + "," + app.maps.y.toString() + ")", canvasWidth - 195, 15);
         ctx.fillText("Mouse(x,y) = (" + app.touches.mouseX.toString() + "," + app.touches.mouseY.toString() + ")", canvasWidth - 195, 30);
         if (Utils.isDefined(app.entities.selectedEntity)) {
             ctx.fillText("Entity = " + app.entities.selectedEntity.name + "(" + app.entities.selectedEntity.x + "," + app.entities.selectedEntity.y + ")", canvasWidth - 195, 45);
