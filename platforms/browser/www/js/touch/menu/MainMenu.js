acc79178ce4d142bc78d95ad6e34981cb5c1e72c
Jean jean <jean.haderer@keyconsulting.fr>
2016-11-10 10:40:39 +0100

    no more www in platform

--- platforms/browser/www/js/touch/menu/MainMenu.js
+++
@@ -1,8 +0,0 @@
-function MainMenu() {
-    console.log("Create main menu.");
-    Window.apply(this, "mainMenu", "UI/background", 0, 0);
-    this.addElement("UI/createElementButton", "", 10, 10);
-    this.addElement("UI/createRoomButton", "", 10, 40);
-}
-
-MainMenu.prototype = new Window();
